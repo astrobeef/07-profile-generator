207aaa035f73c505f53001d366d0023c702be3c3
Brian Graf <brian.g.graf@gmail.com>
2019-11-09 16:57:22 -0700

    Added HTML template and finished functionality

--- index.js
+++ index.js
@@ -7,37 +7,28 @@ const writeFile = util.promisify(fs.writeFile);
 const readFile = util.promisify(fs.readFile);
 
 const octokit = Octokit({
-    userAgent : "Profile generator",
-    baseUrl : "https://api.github.com",
+    userAgent: "Profile generator",
+    baseUrl: "https://api.github.com",
 })
 
 const user = {
-    img_src : "http://placehold.jp/200x200.png",
-    username : "test name",
-    links : {
-        github : "https://github.com/astrobeef",
-        location : "https://www.google.com/maps?q=phoenix+google+maps&rlz=1C1CHBF_enUS869US869&um=1&ie=UTF-8&sa=X&ved=0ahUKEwj7pJiS7dvlAhVkGDQIHWUrBYIQ_AUIEigB",
-        blog : "https://astrobeef.github.io/Responsive-Portfolio-w-Bootstrap/index.html"
-    },
-    bio : "test bio",
-    repository_count : 0,
-    follower_count : 0,
-    GHStars_count : 0,
-    following_count : 0
+    img_src: "http://placehold.jp/200x200.png",
+    username: "test name",
+    github : " ",
+    location : " ",
+    blog : " ",
+    bio: "test bio",
+    repository_count: 0,
+    follower_count: 0,
+    GHStars_count: 0,
+    following_count: 0
 }
 
 var data;
 
-let template = `
-<h1> Hello my name is {{ name }}
-`;
+let template = readFile("index_template.html", "utf8", function(err){if(err)throw(err)});
 
-template = replacePlaceholder(template, "name", user.username);
-
-writeFile("index.html", template);
-console.log("Wrote template to file : index.html");
-
-function replacePlaceholder(template, target, value){
+function replacePlaceholder(template, target, value) {
     console.log("-Replacing placeholder, " + target + ", with the value of " + value);
     const regex = new RegExp("{{ " + target + " }}", "gm");
     const newTemplate = template.replace(regex, value);
@@ -46,17 +37,43 @@ function replacePlaceholder(template, target, value){
     return newTemplate;
 }
 
-async function getData()
-{
+async function getData() {
+    console.log("-Fetching user data...");
     data = await octokit.users.getByUsername(
         {
-            username : "astrobeef"
+            username: "astrobeef"
         }
     )
-    
-    console.log(data);
+
+    data = data.data;
+
+    user.username = data.login;
+    user.bio = data.bio;
+    user.follower_count = data.followers;
+    user.following_count = data.following;
+    user.img_src = data.avatar_url;
+    user.GHStars_count = data.starred_url;
+    user.blog = data.blog;
+    user.github = data.url;
+    user.location = data.location;
+    user.repository_count = data.public_repos;
+
+    console.log(`-Retrieved user data : ${JSON.stringify(user)}`);
+    console.log("-Finished getting data");
+    console.log("----------------------");
+
+    generateHTML();
+}
+
+function generateHTML() {
+    for (key in user) {
+        template = replacePlaceholder(template, key, user[key]);
+    }
+
+    writeFile("index.html", template);
+    console.log("Wrote template to file : index.html");
+    console.log("-----------------------------------")
+
 }
 
 getData();
-
-console.log(data);
